6f294df24070aba947b93892a391d280bd3f9049
tyler <tylerjyoungdahl@gmail.com>
2024-10-16 18:43:05 -0700

    add Ship class

---
+++ src/logic.js
@@ -0,0 +1,17 @@
+class Ship {
+  constructor(length, hits, sunk) {
+    this.length = length;
+    this.hits = hits;
+    this.sunk = false;
+  }
+
+  hit() {
+    hits++;
+  }
+
+  isSunk() {
+    if (this.hits === this.length) {
+      this.sunk = true;
+    }
+  }
+}
